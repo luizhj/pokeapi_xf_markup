dfe448015edd3dad37ad40ff3c9632b3aac3b55f
luizhj <luizhj@gmail.com>
2020-05-11 11:42:56 -0300

    hello github

---
+++ src/pokeapi_xf_markup.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace pokeapi_xf_markup.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new pokeapi_xf_markup.App());
+        }
+    }
+}
---
+++ src/pokeapi_xf_markup/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace pokeapi_xf_markup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Device.SetFlags(new string[] { "Markup_Experimental" });
+            MainPage = new MainPage();
+        }
+    }
+}
